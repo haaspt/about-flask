65c4d2c75deb36227218f7cb0e92e33741541288
Tyler Haas <Tyler.Haas@roomtoread.org>
2018-08-08 16:49:24 -0700

    Add: site constants

--- AboutApp/views.py
+++ AboutApp/views.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 from AboutApp import app
 
+AUTHOR = "Patrick Tyler Haas"
+SITEURL = "literallyjustgraphs.com"
+SITENAME = "Patrick Tyler Haas"
+SITESUBTITLE = "A little about me"
+
 @app.route("/")
 def main():
     return render_template("index.html", title="Test!")

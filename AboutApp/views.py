9e3f52a364f94d9bf15fad7b89d9251a8475b956
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2018-08-10 23:39:48 -0700

    Fix: page titles

--- AboutApp/views.py
+++ AboutApp/views.py
@@ -7,9 +7,10 @@ SITENAME = "Patrick Tyler Haas"
 
 @app.route("/")
 def about_me():
-    const_dict = {
+    attr_dict = {
         "AUTHOR": AUTHOR,
         "SITEURL": SITEURL,
+        "title": "PTH — Portfolio",
         "description": "My personal portfolio and developer's blog"
     }
 
@@ -25,10 +26,17 @@ def about_me():
          "project_link": "#"}
     ]
 
-    return render_template("about.html", projects=projects, **const_dict)
+    return render_template("about.html", projects=projects, **attr_dict)
 
 @app.route("/contact")
 def contact():
 
-    return render_template("contact.html")
+    attr_dict = {
+        "AUTHOR": AUTHOR,
+        "SITEURL": SITEURL,
+        "title": "PTH — Contact",
+        "description": "My personal portfolio and developer's blog"
+    }
+
+    return render_template("contact.html", **attr_dict)
 

06a20535f61931115475674d7a02a88a6ad6d993
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2018-08-10 22:46:26 -0700

    Refac: changed file structure

---
+++ AboutApp/static/scripts/main.js
@@ -0,0 +1,8 @@
+
+function highlightOuterElement(obj) {
+    obj.parentNode.parentNode.style.borderColor = "#d10007";
+}
+
+function removeHighlightOuterElement(obj) {
+    obj.parentNode.parentNode.style.borderColor = "#313131";
+}
